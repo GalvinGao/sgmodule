97db97b3f23d8a117d15ac60ffe5155ef5fac393
GalvinGao <me@galvingao.com>
2024-11-09 21:00:46 +0800

    fix: rotaenolocalreflect-capture

--- rotaenolocalreflect-capture.js
+++ rotaenolocalreflect-capture.js
@@ -1,3 +1,10 @@
+
+// make sure the request is a GET
+if ($request.method !== "GET") {
+    $done({});
+    return;
+}
+
 let body = $response.body;
 $persistentStore.write(body, "rotaenolocalreflect.CloudSave");
 $done({});
